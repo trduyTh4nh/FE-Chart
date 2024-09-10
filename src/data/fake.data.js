ff264e79494251ea91c9bf592eff06b6d508dfe6
Duy Thanh <tttranduy999@gmail.com>
2024-09-10 18:50:57 +0700

    update some layout

--- src/data/fake.data.js
+++ src/data/fake.data.js
@@ -5,7 +5,7 @@ const dataCategory = [
     { id: 1, name_cate: "A", color: "#E87722" },
     { id: 2, name_cate: "B", color: "#6ECEB2" },
     { id: 3, name_cate: "C", color: "#FED141" },
-
+    { id: 4, name_cate: "D", color: "#FED141" },
 ];
 
 const dataSample = Array.from({ length: 20 }, (_, index) => {
@@ -13,14 +13,13 @@ const dataSample = Array.from({ length: 20 }, (_, index) => {
     date.setDate(date.getDate() - (5 - index) * 10);
 
     const formattedDate = `${date.getDate().toString().padStart(2, '0')}/${(date.getMonth() + 1).toString().padStart(2, '0')}/${date.getFullYear()}`;
-    
     // const category = getRandomElement(dataCategory); 
     return {
         month: formattedDate,
         A: getRandomInt(50, 250),
         B: getRandomInt(50, 250),
         C: getRandomInt(50, 250),
-       
+        D: getRandomInt(50, 250),
     };
 });
 
